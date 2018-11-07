e2c0963f450b3b018d0431a36c25dfb9a539b9e5
dolow <velvet.nanashi@gmail.com>
2018-11-07 19:38:13 +0900

    fixed mkdir bug

--- packages/scene-graph-mediator/client/src/cli.ts
+++ packages/scene-graph-mediator/client/src/cli.ts
@@ -32,25 +32,43 @@ export default function cli(): void {
   // scene graph file manages assets
   const exportExportMap = manager.exportAsset(sceneGraphs, args.runtime, args.assetRoot, args.assetDestDir, args.assetNameSpace);
 
-  // file system control
-  fs.mkdir(args.assetDestDir, () => {
-    // write scene graph file
-    sceneGraphs.forEach((sceneGraph, sceneFilePath) => {
-      const destFileName = path.basename(sceneFilePath) + '.json';
-      const dest = path.join(args.assetDestDir, destFileName);
-      // if (debug) {
-      fs.writeFile(dest, JSON.stringify(sceneGraph, null, 2), () => {});
-      // } else {
-      //   fs.writeFile(dest, JSON.stringify(sceneGraph), () => {});
-      // }
-    });
+  let newDirRoot = args.assetDestDir;
+  const directories: string[] = newDirRoot.split(path.sep);
+  const makingDirectories: string[] = [];
 
-    // copy assets
-    exportExportMap.forEach((entity) => {
-      const targetDir = path.dirname(entity.localDestPath);
-      fs.mkdir(targetDir, () => {
-        fs.copyFile(entity.localSrcPath, entity.localDestPath, () => {});
-      });
-    });
+  // create dest directory recursively
+  while (!fs.existsSync(newDirRoot)) {
+    const dir = directories.pop();
+    if (!dir) break;
+    makingDirectories.push(dir);
+    newDirRoot = directories.join(path.sep);
+  }
+
+  while (makingDirectories.length > 0) {
+    const dir = makingDirectories.pop();
+    if (!dir) break;
+    newDirRoot = path.join(newDirRoot, dir);
+    fs.mkdirSync(newDirRoot);
+  }
+
+  // write scene graph file
+  sceneGraphs.forEach((sceneGraph, sceneFilePath) => {
+    const destFileName = path.basename(sceneFilePath) + '.json';
+    const dest = path.join(args.assetDestDir, destFileName);
+    // if (debug) {
+    fs.writeFile(dest, JSON.stringify(sceneGraph, null, 2), () => {});
+    // } else {
+    //   fs.writeFile(dest, JSON.stringify(sceneGraph), () => {});
+    // }
+  });
+
+  // copy assets
+  exportExportMap.forEach((entity) => {
+    const targetDir = path.dirname(entity.localDestPath);
+    if (!fs.existsSync(targetDir)) {
+      fs.mkdirSync(targetDir);
+    }
+
+    fs.copyFile(entity.localSrcPath, entity.localDestPath, () => {});
   });
 }
