f355ccae7d793566f17a8defcbd9eb2b96215089
dolow <velvet.nanashi@gmail.com>
2018-12-25 10:11:52 +0900

    added static config parse process and config file example

--- packages/scene-graph-mediator/client/lib/modules/parseArgs.js
+++ packages/scene-graph-mediator/client/lib/modules/parseArgs.js
@@ -1,28 +1,54 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var path = require("path");
+var parseCliOption = require("command-line-args");
 /**
  * Parses CLI argument via process.env and convert to Args type.
  */
 function parseArgs() {
+    var argsDefinitions = [
+        { name: 'config', alias: 'c', type: String },
+        { name: 'runtime', alias: 'r', type: String },
+        { name: 'assetRoot', alias: 'a', type: String },
+        { name: 'sceneFiles', alias: 's', type: String, multiple: true },
+        { name: 'destDir', alias: 'd', type: String },
+        { name: 'assetDestDir', alias: 'g', type: String },
+        { name: 'assetNameSpace', alias: 'n', type: String },
+        { name: 'plugins', alias: 'p', type: String, multiple: true },
+    ];
+    var nodeOptions = parseCliOption(argsDefinitions);
+    // passing option via process.env is deprecated
+    var config = {};
+    if (nodeOptions.config) {
+        var userConfigFactory = require(path.resolve(process.cwd(), nodeOptions.config));
+        config = userConfigFactory();
+        if (config.sceneFiles && !Array.isArray(config.sceneFiles)) {
+            config.sceneFiles = [config.sceneFiles];
+        }
+        if (config.plugins && !Array.isArray(config.plugins)) {
+            config.plugins = [config.plugins];
+        }
+    }
+    // priority
+    // cli option > user config > env
     var args = {
-        runtime: process.env.RUNTIME || '',
-        assetRoot: process.env.ASSET_ROOT || '',
-        sceneFiles: process.env.SCENE_FILE ? process.env.SCENE_FILE.split(' ') : [],
-        destDir: process.env.DEST || path.resolve(process.cwd(), 'scene-graph'),
-        assetDestDir: process.env.ASSET_DEST || '',
-        assetNameSpace: process.env.ASSET_NAME_SPACE || 'assets',
-        plugins: process.env.PLUGINS ? process.env.PLUGINS.split(' ') : []
+        runtime: nodeOptions.runtime || config.runtime || process.env.RUNTIME || '',
+        assetRoot: nodeOptions.assetRoot || config.assetRoot || process.env.ASSET_ROOT || '',
+        sceneFiles: nodeOptions.sceneFiles || config.sceneFiles || (process.env.SCENE_FILE ? process.env.SCENE_FILE.split(' ') : []),
+        destDir: nodeOptions.destDir || config.destDir || process.env.DEST || path.resolve(process.cwd(), 'scene-graph'),
+        assetDestDir: nodeOptions.assetDestDir || config.assetDestDir || process.env.ASSET_DEST || '',
+        assetNameSpace: nodeOptions.assetNameSpace || config.assetNameSpace || process.env.ASSET_NAME_SPACE || 'assets',
+        plugins: nodeOptions.plugins || config.plugins || (process.env.PLUGINS ? process.env.PLUGINS.split(' ') : [])
     };
     args.assetDestDir = args.assetDestDir || path.resolve(args.destDir, args.assetNameSpace);
     if (!args.runtime) {
-        throw new Error();
+        throw new Error('runtime option is required');
     }
     if (!args.assetRoot) {
-        throw new Error();
+        throw new Error('assetRoot option is required');
     }
     if (args.sceneFiles.length === 0) {
-        throw new Error();
+        throw new Error('sceneFiles option is required');
     }
     if (!path.isAbsolute(args.assetRoot)) {
         args.assetRoot = path.resolve(process.cwd(), args.assetRoot);
--- packages/scene-graph-mediator/client/src/modules/parseArgs.ts
+++ packages/scene-graph-mediator/client/src/modules/parseArgs.ts
@@ -1,30 +1,69 @@
 import * as path from 'path';
+import * as parseCliOption from 'command-line-args';
 import Args from '../interface/Args';
 
 /**
  * Parses CLI argument via process.env and convert to Args type.
  */
 export default function parseArgs(): Args {
+  const argsDefinitions = [
+    { name: 'config',         alias: 'c', type: String },
+    { name: 'runtime',        alias: 'r', type: String },
+    { name: 'assetRoot',      alias: 'a', type: String },
+    { name: 'sceneFiles',     alias: 's', type: String, multiple: true },
+    { name: 'destDir',        alias: 'd', type: String },
+    { name: 'assetDestDir',   alias: 'g', type: String },
+    { name: 'assetNameSpace', alias: 'n', type: String },
+    { name: 'plugins',        alias: 'p', type: String, multiple: true },
+  ];
+
+  const nodeOptions = parseCliOption(argsDefinitions);
+
+  // passing option via process.env is deprecated
+
+  let config: {
+    runtime?: string;
+    assetRoot?: string;
+    sceneFiles?: string | string[];
+    destDir?: string;
+    assetDestDir?: string;
+    assetNameSpace?: string;
+    plugins?: string | string[];
+  } = {};
+
+  if (nodeOptions.config) {
+    const userConfigFactory = require(path.resolve(process.cwd(), nodeOptions.config));
+    config = userConfigFactory();
+    if (config.sceneFiles && !Array.isArray(config.sceneFiles)) {
+      config.sceneFiles = [config.sceneFiles];
+    }
+    if (config.plugins && !Array.isArray(config.plugins)) {
+      config.plugins = [config.plugins];
+    }
+  }
+
+  // priority
+  // cli option > user config > env
   const args: Args = {
-    runtime:        process.env.RUNTIME    || '',
-    assetRoot:      process.env.ASSET_ROOT || '',
-    sceneFiles:     process.env.SCENE_FILE ? process.env.SCENE_FILE.split(' ') : [],
-    destDir:        process.env.DEST       || path.resolve(process.cwd(), 'scene-graph'),
-    assetDestDir:   process.env.ASSET_DEST || '',
-    assetNameSpace: process.env.ASSET_NAME_SPACE || 'assets',
-    plugins:        process.env.PLUGINS ? process.env.PLUGINS.split(' ') : []
+    runtime:        nodeOptions.runtime        || config.runtime        || process.env.RUNTIME || '',
+    assetRoot:      nodeOptions.assetRoot      || config.assetRoot      || process.env.ASSET_ROOT || '',
+    sceneFiles:     nodeOptions.sceneFiles     || config.sceneFiles     || (process.env.SCENE_FILE ? process.env.SCENE_FILE.split(' ') : []),
+    destDir:        nodeOptions.destDir        || config.destDir        || process.env.DEST       || path.resolve(process.cwd(), 'scene-graph'),
+    assetDestDir:   nodeOptions.assetDestDir   || config.assetDestDir   || process.env.ASSET_DEST || '',
+    assetNameSpace: nodeOptions.assetNameSpace || config.assetNameSpace || process.env.ASSET_NAME_SPACE || 'assets',
+    plugins:        nodeOptions.plugins        || config.plugins        || (process.env.PLUGINS ? process.env.PLUGINS.split(' ') : [])
   };
 
   args.assetDestDir = args.assetDestDir || path.resolve(args.destDir, args.assetNameSpace);
 
   if (!args.runtime) {
-    throw new Error();
+    throw new Error('runtime option is required');
   }
   if (!args.assetRoot) {
-    throw new Error();
+    throw new Error('assetRoot option is required');
   }
   if (args.sceneFiles.length === 0) {
-    throw new Error();
+    throw new Error('sceneFiles option is required');
   }
 
   if (!path.isAbsolute(args.assetRoot)) {
