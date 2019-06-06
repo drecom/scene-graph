25be12a89aea6348af2f5a62df4913d41fb03ea0
Egliss <syunya.tokunaga@drecom.co.jp>
2019-06-06 10:45:06 +0900

    Remove unuse module and danger codes

--- packages/scene-graph-mediator/client/lib/modules/fillText.d.ts
+++
@@ -1,8 +0,0 @@
-export {};
-declare global {
-    interface String {
-        fillRight(word: string, count: number): string;
-        fillLeft(word: string, count: number): string;
-    }
-}
-export default function dummy(): void;
