2f6ced4ce0c80bc8e8de8de0fec02d5a5186a718
takeshi-sakurai <takeshi.sakurai@drecom.co.jp>
2019-06-07 15:35:25 +0900

    add comment about when multiple plugin set.

--- packages/scene-graph-mediator/runtime/src/interface/ImporterPlugin.ts
+++ packages/scene-graph-mediator/runtime/src/interface/ImporterPlugin.ts
@@ -2,7 +2,10 @@ import { SchemaJson, Node } from '@drecom/scene-graph-schema';
 import { ImportOption } from 'importer/Importer';
 
 export default interface ImporterPlugin {
+  // Use if you want to customize the creation of runtime objects.
+  // When multiple plug-ins are set, the one registered first is prioritized.
   createRuntimeObject?(node: Node, resources: any): any | null;
+
   extendRuntimeObjects(
     schema: SchemaJson,
     nodeMap: Map<string, Node>,
